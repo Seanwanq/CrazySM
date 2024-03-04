d18890a996d6af8aa553647dc60f15c6666b8f89
Sean Wang <seanwang313@outlook.com>
2024-03-04 19:46:31 +0100

    Added gitignore

--- Week2_Exercise4/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/seanw/OneDrive/Files/Master EXPH/Master Year 1/Modeling and Simulation/homework/Week2_Exercise4
-# Build directory: C:/Users/seanw/OneDrive/Files/Master EXPH/Master Year 1/Modeling and Simulation/homework/Week2_Exercise4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
